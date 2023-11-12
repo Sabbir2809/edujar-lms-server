68372cab9d35e8cf7d3980352e2435787194d118
mma-tahmid <mma.tahmid@gmail.com>
2023-11-12 19:56:39 +0600

    my Commit

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,19 @@
 // Dependencies
 const express = require("express");
+const categoryControllers = require("../controllers/categoryController")
 const router = express.Router();
+
+// categoryControolers=require
 
 // API Routing End Point:
 // Route Task: Tahmid
+
+//Category Controller
+
+
+router.get("/all-categories", categoryControllers.allCategories)
+
+
 
 // Exports
 module.exports = router;
