4b7dfdb3ed2f00b783578400a6f03228e3528c77
Ebadzmne <ebadzmn1@gmail.com>
2023-11-12 00:16:25 -0700

    category controller done

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,1 +1,12 @@
 // Category task: Ebad
+
+const categoryModel = require("../model/categoryModel");
+
+const AllCategories = async (req, res) => {
+    try {
+        const categories = await categoryModel.find();
+        res.status(200).json(categories);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
