8d7c4a49e71a9dd915015fc2ca2e7ebd05a42f1c
mma-tahmid <mma.tahmid@gmail.com>
2023-11-13 11:00:40 +0600

    course route added

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,8 @@
 // Dependencies
 const express = require("express");
 const categoryControllers = require("../controllers/categoryController")
+const courseController = require("../controllers/categoryController")
+
 const router = express.Router();
 
 // categoryControolers=require
@@ -12,6 +14,9 @@ const router = express.Router();
 
 
 router.get("/all-categories", categoryControllers.allCategories)
+router.post("/create-course", upload.single("thumbnail"), courseController.createCourse);
+router.get("/allCourses", courseController.getAllCourse)
+// router.post("/update-course/:id",courseController.UpdateCourse)
 
 
 
