a64be2e821acad987456ee3b1395cecafc572bf0
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-11-13 12:15:48 +0600

    [Bug Fixed]: Code Review and Testing

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,16 @@
 // Dependencies
 const express = require("express");
+const categoryController = require("../controllers/categoryController");
+const courseController = require("../controllers/courseController");
+const upload = require("../utility/multerConfig");
 const router = express.Router();
 
-// API Routing End Point:
-// Route Task: Tahmid
+// Public API Routing Endpoint
+router.get("/all-category", categoryController.getAllCategory);
+router.get("/all-course", courseController.getAllCourse);
+
+// Private API Routing Endpoint
+router.post("create-course", upload.single("thumbnail"), courseController.createCourse);
 
 // Exports
 module.exports = router;
