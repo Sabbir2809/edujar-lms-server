f80d8214143100a34e620d4ae0ad6529af760c0e
Rasel <mdraselkibria11@gmail.com>
2023-11-22 12:06:00 +0600

    cloudinary storage

--- src/utility/cloudinaryStorage.js
+++ src/utility/cloudinaryStorage.js
@@ -4,7 +4,7 @@ const cloudinary = require("../utility/cloudinaryConfig");
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: "lesson_videos", // Set the folder name where the video will be stored
+        folder: "edujar/lesson_videos", // Set the folder name where the video will be stored
         resource_type: "video", // Specify the resource type
         allowed_formats: ["mp4", "webm"], // Specify allowed video formats
         use_filename: true, // Use the original file name as the Cloudinary public ID
