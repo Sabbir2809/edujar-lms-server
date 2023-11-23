5ad2196ac36d2abbd7f25b5bbc9122115a4760ad
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-11-23 13:29:21 +0600

    [Deploy]: Server Host on Render

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const hpp = require("hpp");
 const mongoSanitize = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 const router = require("./src/routes/api");
 
@@ -13,6 +14,13 @@ app.use(cors());
 app.use(helmet());
 app.use(hpp());
 app.use(mongoSanitize());
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,7 +29,7 @@ app.use("/api/v1", router);
 
 // Health API
 app.get("/", (req, res) => {
-  res.status(200).send("Edujar LMS API: All is Well");
+  res.status(200).send("Edujar LMS REST API: All is Well");
 });
 
 // Exports
