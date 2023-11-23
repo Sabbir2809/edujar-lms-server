3e06150f0cb2bcb083fdfd18409bf5663fdaf707
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-11-23 13:19:59 +0600

    [Deploy]: Server Host on Render

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.use(mongoSanitize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/api/v1", router);
+
 // Health API
 app.get("/", (req, res) => {
   res.status(200).send("Edujar LMS API: All is Well");
 });
-
-// Routes
-app.use("/api/v1", router);
 
 // Exports
 module.exports = app;
